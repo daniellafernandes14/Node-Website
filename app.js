da7348e60779599a95e0088bc1290f38b17e0b2c
Daniella Fernandes <fernandesdaniella@hotmail.com>
2023-06-07 15:05:14 +0100

    Refactored code and moved routers into own router file

--- app.js
+++ app.js
@@ -2,30 +2,16 @@ const express = require('express');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
-const sessions = require('./src/data/sessions.json');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-const sessionsRouter = express.Router();
+const sessionsRouter = require('./src/routers/sessionsRouter');
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
-
-sessionsRouter.route('/')
-  .get((req, res) => {
-    res.render('sessions', {
-      sessions,
-    });
-  })
-
-sessionsRouter.route('/:id')
-  .get((req, res) => {
-    const id = req.params.id
-;    res.send('hello single sessions ' + id);
-  })
 
 app.use('/sessions', sessionsRouter);
 
