c4a0d1d4dd5af610838330336eeaaf72ed5d7774
Daniella Fernandes <fernandesdaniella@hotmail.com>
2023-06-07 16:22:07 +0100

    Refactoring code

--- src/routers/sessionsRouter.js
+++ src/routers/sessionsRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const sessionsRouter = express.Router();
 const debug = require('debug')('app:sessionsRouter');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const sessions = require('../data/sessions.json');
 require('dotenv').config();
 
@@ -20,20 +20,41 @@ sessionsRouter.route('/')
 
         const db = client.db(dbName);
 
-        const sessions = await db.collection('sessions').find().toArray();
+        const sessions = await db
+          .collection('sessions')
+          .find()
+          .toArray();
         res.render('sessions', { sessions });
       } catch (error) {
         debug(error.stack);
       }
-    }())
+      client.close();
+    })();
   })
 
 sessionsRouter.route('/:id')
   .get((req, res) => {
     const id = req.params.id;
-    ; res.render('session', {
-      session: sessions[id],
-    });
+    const url = `mongodb+srv://fernandesdaniella:${password}@daniellatools.kljessr.mongodb.net/?retryWrites=true&w=majority`
+    const dbName = 'daniellaTools';
+
+    (async function mongo() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        debug('Connected to the Mongo DB');
+
+        const db = client.db(dbName);
+
+        const session = await db
+          .collection('sessions')
+          .findOne({ _id: new ObjectId(id) });
+        res.render('session', {session});
+      } catch (error) {
+        debug(error.stack);
+      }
+      client.close();
+    }())
   })
 
   module.exports = sessionsRouter;
