6a4942aedae6d3d266bf145a63e0335411907996
Daniella Fernandes <fernandesdaniella@hotmail.com>
2023-06-07 12:27:11 +0100

    Added packages and created index.html file

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/', (req, res) => {
   res.send('Hello from my app');
 })
 
-app.listen(3000, () => {
-  debug('Listening on port 3000');
+app.listen(PORT, () => {
+  debug(`Listening to port ${PORT}`);
 });
